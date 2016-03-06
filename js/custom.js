64778c1102049050446a712e0656eeb4155fd770
Bijiabo <bijiabo@gmail.com>
2016-03-06 18:16:09 +0800

    优化性能;

--- js/custom.js
+++ js/custom.js
@@ -97,6 +97,11 @@ var _AnnulusRemoteController = function(keyboardQueryString, keysContainerQueryS
     this.activeButtonStatusClassName = "active"; //活跃按钮 CSS 类名
     this.currentActiveButtonKeyName = ""; //当前活跃按钮的按键名
 
+    // 当前控件状态
+    this.status = {
+        touchMode: this.touchAction.none
+    };
+
     // MARK: - bind events
 
     $(document).on(this.touchAction.start, keyboardQueryString, function(event){
@@ -120,6 +125,7 @@ var _AnnulusRemoteController = function(keyboardQueryString, keysContainerQueryS
     });
 
     $(document).on(this.touchAction.move, keyboardQueryString, function(event){
+        self.updateTouchAction(self.touchAction.move);
         var element = $(this);
         self.updateCurrentTouchButton(element, event);
     });
@@ -127,6 +133,11 @@ var _AnnulusRemoteController = function(keyboardQueryString, keysContainerQueryS
     this.tapFunction = function(keyName){
         console.log(new Date());
         console.log("Default tap function. KeyName:"+keyName);
+    };
+
+    this.longTapFunction = function(keyName){
+        console.log(new Date());
+        console.log("Default long tap function. KeyName:"+keyName);
     };
 
 };
@@ -148,7 +159,8 @@ _AnnulusRemoteController.prototype.setTapFunction = function(func) {
 _AnnulusRemoteController.prototype.touchAction = {
     start: "touchstart",
     end: "touchend",
-    move: "touchmove"
+    move: "touchmove",
+    none: "none"
 };
 
 _AnnulusRemoteController.prototype.updateTouchAction = function(action) {
@@ -156,19 +168,25 @@ _AnnulusRemoteController.prototype.updateTouchAction = function(action) {
         case this.touchAction.start:
             this.touchTime.start = (new Date()).getTime();
             this.touchTime.duration = 0;
+            this.status.touchMode = this.touchAction.start;
             break;
         case this.touchAction.end:
             this.touchTime.end = (new Date()).getTime();
             this.touchTime.duration = this.touchTime.end - this.touchTime.start;
+            this.status.touchMode = this.touchAction.none;
+            break;
+        case this.touchAction.move:
+            this.status.touchMode = this.touchAction.move;
             break;
         default:
+            this.status.touchMode = this.touchAction.none;
             break;
     }
 };
 
 _AnnulusRemoteController.prototype.setStatusToActiveForButtonByKeyName = function(keyName) {
 
-    if (isEmptyForString(this.currentActiveButtonKeyName)) {
+    if (isEmptyForString(keyName)) {
         this.currentActiveButtonKeyName = keyName;
         this.clearCurrentActiveButtonState();
         return;
@@ -177,6 +195,14 @@ _AnnulusRemoteController.prototype.setStatusToActiveForButtonByKeyName = functio
     var targetButton = this.buttonForKeyName(keyName);
 
     if (targetButton.length===0) {return;}
+
+    //console.log("this.currentActiveButtonKeyName: "+this.currentActiveButtonKeyName+", keyName:"+keyName);
+    //console.log(this.status.touchMode);
+
+    if (this.currentActiveButtonKeyName===keyName &&
+        (this.status.touchMode===this.touchAction.move||this.status.touchMode===this.touchAction.end)) {
+        return;
+    }
     if (this.isActiveForButton(targetButton)) {return;}
 
     this.clearCurrentActiveButtonState();
@@ -189,6 +215,7 @@ _AnnulusRemoteController.prototype.isActiveForButton = function(button) {
 };
 
 _AnnulusRemoteController.prototype.buttonForKeyName = function(keyName) {
+    if (isEmptyForString(keyName)) {return [];}
     return this.keysContainer.find("#keys-box .key[key-name="+keyName+"], .center-key[key-name="+keyName+"]");
 };
 
@@ -210,28 +237,30 @@ _AnnulusRemoteController.prototype.updateCurrentTouchButton = function(element, 
     var distanceToCenter = Math.sqrt( Math.pow(currentTargetPosition.x - elementCenter.x, 2) + Math.pow(currentTargetPosition.y - elementCenter.y, 2) );
     var angleToCenter = Math.atan2(elementCenter.y - currentTargetPosition.y, elementCenter.x - currentTargetPosition.x) * 180 / Math.PI
 
+    var targetActiveButton = "";
+
     if (distanceToCenter > this.keysContainerRadius) {
-        this.currentActiveButtonKeyName = "";
+        targetActiveButton = "";
     } else if (distanceToCenter > this.centerKeyRadius) {
         switch(true) {
             case -45<angleToCenter && angleToCenter<=45:
-                this.currentActiveButtonKeyName = "left";
+                targetActiveButton = "left";
                 break;
             case 45<angleToCenter && angleToCenter<=135:
-                this.currentActiveButtonKeyName = "up";
+                targetActiveButton = "up";
                 break;
             case 135<angleToCenter || angleToCenter<=-135:
-                this.currentActiveButtonKeyName = "right";
+                targetActiveButton = "right";
                 break;
             case -135<angleToCenter && angleToCenter<=-45:
-                this.currentActiveButtonKeyName = "down";
+                targetActiveButton = "down";
                 break;
             default:
                 break;
         }
     } else {
-        this.currentActiveButtonKeyName = "center";
+        targetActiveButton = "center";
     }
 
-    this.setStatusToActiveForButtonByKeyName(this.currentActiveButtonKeyName);
+    this.setStatusToActiveForButtonByKeyName(targetActiveButton);
 };
